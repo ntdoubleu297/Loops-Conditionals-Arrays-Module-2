915cf0fa49ef4a208d1a9b8580bad2c751748d9d
Neil T. White <87203253+ntdoubleu297@users.noreply.github.com>
2021-08-03 14:26:31 -0400

    Add files via upload

---
+++ oldenough2votecode.js
@@ -0,0 +1,27 @@
+/*
+Instructions
+
+Create a conditional that checks if you're old enough to vote.
+- isOldEnoughToVote(age)
+- response will store true or false values
+
+*/
+let response;
+
+return result;// Add your code here
+function isOldEnoughToVote(age) {
+  if(age >= 18){
+    result = 'true'
+  }else{
+   result = 'false'
+  }
+   //your code goes here
+  // return true or false based on the age
+}
+
+//open the browser console to check results
+console.log('results: ', isOldEnoughToVote(18));
+// Don't edit the code below here
+if (typeof module !== 'undefined') {
+  module.exports = isOldEnoughToVote;
+}
